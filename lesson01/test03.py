978f2787dbd08abd962d00a91ceb6de548eddab6
mayomayo25 <10bvb25hal45016@gmail.com>
2019-07-25 19:59:26 +0900

    sec3-10

---
+++ lesson01/test03.py
@@ -0,0 +1,17 @@
+num1 = 10 + 2
+num2 = 50 - 5 * 6
+num3 = 8 / 5
+num4 = 5 ** 5
+num5 = num1 * num2
+num6 = 17 / 3
+num7 = 17 // 3
+type1 = type(num3)
+
+print('num1は', num1)
+print('num2は', num2)
+print('num3は', num3)
+print('num4は', num4)
+print('num5は', num5)
+print('num6は', num6)
+print('num7は', num7)
+print('type1は', type1)
