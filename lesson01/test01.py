7017cc35a6b5053a0df76debab72a4dcd81a42ff
mayomayo25 <10bvb25hal45016@gmail.com>
2019-07-25 19:36:35 +0900

    sec3-8

---
+++ lesson01/test01.py
@@ -0,0 +1,15 @@
+num   = 1
+name  = 'hogehoge_no_okina'
+is_ok = True
+
+message = 'hello world!'
+message = name
+
+age     = '24'
+new_age = int(age)
+
+print(num, type(num))
+print(name, type(name))
+print(is_ok, type(is_ok))
+print(message, type(message))
+print(new_age, type(new_age))
